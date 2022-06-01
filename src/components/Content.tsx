1144ea2192cccd83d688dfe34faeb86ec1c454bc
GuilhermeFRocha <guilhermefdarocha@outlook.com>
2022-06-01 19:31:58 -0300

    project

---
+++ src/components/Content.tsx
@@ -0,0 +1,35 @@
+import { MovieProps } from "../App";
+import { MovieCard } from "./MovieCard";
+
+interface ContentProps {
+  selectedGenre: {
+    title: string;
+  };
+  movies: MovieProps[];
+}
+
+export function Content({ movies, selectedGenre }: ContentProps) {
+  return (
+    <div className="container">
+      <header>
+        <span className="category">
+          Categoria:<span> {selectedGenre.title}</span>
+        </span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map((movie) => (
+            <MovieCard
+              key={movie.imdbID}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
+}
---
+++ src/components/SideBar.tsx
@@ -0,0 +1,34 @@
+import { Button } from "./Button";
+import { GenreResponseProps } from "../App";
+
+interface sideBarProps {
+  genres: GenreResponseProps[];
+  handleClickButton: (id: number) => void;
+  selectedGenreId: number;
+}
+
+export function SideBar({
+  genres,
+  handleClickButton,
+  selectedGenreId,
+}: sideBarProps) {
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
+}
